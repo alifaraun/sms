0f25f6f217e87941d9b471b7b8472f3107860c20
alifaraun <ali1996426@hotmail.com>
2017-10-22 00:48:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,334 @@
+<?php include 'header.php'; ?>
+
+<!-- Start carousel-bg -->
+<div class="carousel-bg" id="carousel-bg-img1">
+    <div class="carousel-bg-overly"></div>
+    <div class="carousel-bg-text text-center">
+        You can try the program <span> 20 days </span>
+        <p>We can update the program as you like</p>
+    </div>
+</div>
+<!-- End carousel-bg -->
+
+<!-- Start System functions  -->
+<div class="system-functions text-center">
+    <h1>System functions</h1>
+    <p>With our program you can management</p>
+    <div class="container">
+        <div class="row">   
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/emps.png" />
+                <h4>Employees</h4>
+            </div>  
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/jobs.png" />
+                <h4>Functions</h4>
+            </div>   
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/record_emp.png" />
+                <h4>Attendance Record</h4>
+            </div>    
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/salaries.png" />
+                <h4>Salaries</h4>
+            </div>
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/payment_method.png" />
+                <h4>Payment Methods</h4>
+            </div>
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/categories.png" />
+                <h4>Categories</h4>
+            </div>     
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/products.png" />
+                <h4>Products</h4>
+            </div>        
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/suppliers.png" />
+                <h4>Suppliers</h4>
+            </div> 
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/invoice_purchase.png" />
+                <h4>Purchase invoices</h4>
+            </div>  
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/cust.png" />
+                <h4>Customers</h4>
+            </div>   
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/invoice_sell.png" />
+                <h4>Sales invoices</h4>
+            </div>                   
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/users.png" />
+                <h4>Users</h4>
+            </div> 
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/register.png" />
+                <h4>Register</h4>
+            </div>   
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/sessions.png" />
+                <h4>Sessions</h4>
+            </div>
+            <div class="col-sm-4 col-lg-3 col-xl-2">
+                <img src="img/sys-functions/try_login.png" />
+                <h4>Hack attempt</h4>
+            </div>   
+        </div>
+    </div>
+</div>
+<!-- End System functions  -->
+
+<!-- Start Roles -->
+<div class="container-fluid text-center roles">
+    <div class="row" >
+        <div class="col-sm-2 col-lg-3"></div>
+        <div class="col-sm-8 col-lg-6 roles-title">
+            <h1> Roles </h1>
+        </div>
+        <div class="col-sm-2 col-lg-3"></div>
+    </div><!-- Trigger the Modal -->
+    <img id="myImg" class="img img-fluid" src="img/roles.PNG" alt="Trolltunga, Norway">
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- The Close Button -->
+        <span class="close">&times;</span>
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="img01">
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+</div>
+<!-- End Roles -->
+
+<!-- Start Features -->
+<div class="features text-center">
+    <h1>Features</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-globe fa-4x" ></i>
+                <h4>Works on the network</h4>
+                <p>Many devices can work on the same database at the same time</p>
+            </div>           
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-user-secret fa-4x" ></i>
+                <h4>Detecting hacking attempts</h4>
+                <p>If anyone try to login system and entered password wrong 4 times, it will be register in system (username with passwords and picture of camera if exists)</p>
+            </div>   
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-bell fa-4x" ></i>
+                <h4>Notifications</h4>
+                <p>if there is something important ( products going to be or finished/expired also hacking attempts) you will get notifications on login</p>
+            </div>           
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-lock fa-4x" ></i>
+                <h4>Encrypt passwords</h4>
+                <p>If anyone stole database and tried to read passwords to login to the system ,he/she finds passwords encrypted</p>
+            </div>           
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-clock-o fa-4x" ></i>
+                <h4>Operations log</h4>
+                <p>Register every addition operation in system (like add new product) and who did it and when</p>
+            </div>           
+            <div class="col-sm-6 col-md-4">
+                <i class="fa fa-pencil-square fa-4x" ></i>
+                <h4>Register specific operations</h4>
+                <p>Can register specific operation ( update /delete something and take report ) system and who did it and when</p>
+            </div>           
+        </div>
+    </div>
+</div>
+<!-- End Features -->
+
+<!-- Start pricing -->
+<div class="price text-center">
+    <div class="container">
+        <div class="row" >
+            <div class="col-sm-2 col-lg-3"></div>
+            <div class="col-sm-8 col-lg-6 price-title">
+                <h1>Prices</h1>
+            </div>
+            <div class="col-sm-2 col-lg-3"></div>
+        </div>
+        <div class="row">
+            <div class="col-xl-1"></div>
+            <div class="col-md-12 col-xl-10">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">One Device</h4>
+                                <h1>$200 </h1>
+                                <p class="card-text">You can buy another keys after time with discount</p>
+                                <a href="#" class="btn btn-primary btn-lg"> Buy now </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">2 Devices</h4>
+                                <h1>$350 </h1>
+                                <p class="card-text">You can buy another keys after time with discount</p>
+                                <a href="#" class="btn btn-success btn-lg"> Buy now </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">More than 2 devices</h4>
+                                <h1 class="price-active">$100 </h1>
+                                <p class="card-text">After buy $200 for first device , every another device going to be $100</p>
+                                <a href="#" class="btn btn-danger btn-lg"> Buy now </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-1"></div>
+        </div>
+    </div>
+</div>
+<!-- End pricing -->
+
+<!-- Start Reports -->
+<div class="reports text-center">
+    <div class="container">
+        <h1>Reports</h1>
+        <p>You can get a report for a specific item and a specific date</p>
+        <div class="row text-left">
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Employees</h4>
+                    <li>Information</li>
+                    <li>Salaries</li>
+                    <li>Attendance Record</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Customers</h4>
+                    <li>Information</li>
+                    <li>Purchases</li>
+                    <li>Invoices</li>
+                    <li>Debts</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Suppliers</h4>
+                    <li>Information</li>
+                    <li>Transactions</li>
+                    <li>Invoices</li>
+                    <li>Debts</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Categories</h4>
+                    <li>Information</li>
+                    <li>Profits</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Products</h4>
+                    <li>Information</li>
+                    <li>Finished</li>
+                    <li>Close to finish</li>
+                    <li>Expired</li>
+                    <li>Close to expiry</li>
+                    <li>Profits</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Payment Methods</h4>
+                    <li>Information</li>
+                </ul>
+                <ul class="list-unstyled">
+                    <h4>Functions</h4>
+                    <li>information</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Profits</h4>
+                    <li>Chart with information</li>
+                </ul>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <ul class="list-unstyled">
+                    <h4>Purchase invoices</h4>
+                    <li>information</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Reports -->
+
+<!-- Start Contact us -->
+<div class="contact-us">
+    <div class="container text-center">
+        <div class="contact-us-titlle">
+            <h2> Contact us </h2>
+            <p>If you ever have any questions,suggestions,requests or notes , please do not hesitate to contact us</p>
+        </div>
+        <div class="row text-left">
+            <div class="col-md-4 col-xl-6">
+                <div class="contact-us-notes">
+                    <p>If you want to Evaluation ,Report an error,service, buy program , update program as want or suggest something , please do not hesitate to contact us</p>
+                    <p>Help us to give users better services by send us notes</p>
+                    <p>We want our customers to be happy <i class="fa fa-smile-o"></i></p>
+                </div>
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-map-marker"></i> Address: Libya Tripoli</li>
+                    <li><i class="fa fa-phone"></i> Phone: +32392933838</li>
+                    <li><i class="fa fa-envelope"></i> E-mail:<a href="mailto:info@sysytem.com?subject=Contact"> mail@email.com</a></li>
+                    <li><i class="fa fa-globe"></i> Website:<a href=""> www.alfarouk.com</a></li>
+                </ul>
+            </div>
+            <div class="col-md-8 col-xl-6">
+                <div class="contact-us-warning">* If you would like to receive reply please write your e-mail</div>
+                <form id="main-contact-form" name="contact-form" method="post" action="#">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" maxlength="64" name="name" class="form-control" placeholder="Name or Email" required="required">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <select class="form-control">
+                                <option selected>Choose...</option>
+                                <option>Purchase order</option>
+                                <option>Service request</option>
+                                <option>Evaluation</option>
+                                <option>Report an error</option>
+                                <option>Note</option>
+                                <option>Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" maxlength="64" name="subject" class="form-control" placeholder="Subject" required="required">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="message" maxlength="2000" id="message" class="form-control" rows="6" placeholder="Write your message..." required="required"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Contact us -->
+
+
+<?php include 'footer.php'; ?>
