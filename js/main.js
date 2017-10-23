d70ee67727f44b9f887128c4b36aef7a44bd60f7
alifaraun <ali1996426@hotmail.com>
2017-10-23 23:22:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 
 /* modle roles in main page */
-var modal = document.getElementById('myModal');
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
+function myImg_show() {
+    document.getElementById('myModal').style.display = "block";
+    document.getElementById("img01").src = document.getElementById('myImg').src;
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+function myImg_close() {
+    document.getElementById('myModal').style.display = "none";
 }
 /* End modle roles */
 
 
 /* Start carousel-bg */
-currentimage = 0;
-setInterval(function () {
+currentimage = 1;
+var change_img_bg = setInterval(function () {
+    if (document.getElementById('carousel-bg-doesnot-exist') == null)
+    {
+        clearInterval(change_img_bg);
+        return null;
+    }
     if (currentimage > 2)
     {
         currentimage = 0;
@@ -30,4 +27,36 @@ setInterval(function () {
 }, 3500);
 /* End carousel-bg */
 
+
+/* Start button go to top */
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("btn-to-top").style.display = "block";
+    } else {
+        document.getElementById("btn-to-top").style.display = "none";
+    }
+};
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+/* End button go to top */
+
+
+/* Start FAQ */
+function change_state_faq(question) {
+    if ($(question).next().css('display') == 'block')
+    {
+        $(question).next().hide('slow');
+        $(question).find('i').addClass('fa-plus-circle').removeClass('fa-minus-circle');
+    }
+    else
+    {
+        $(question).next().show('slow');
+        $(question).find('i').addClass('fa-minus-circle').removeClass('fa-plus-circle');
+    }
+}
+/* End FAQ */
 
